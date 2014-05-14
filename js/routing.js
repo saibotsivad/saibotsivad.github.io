8d4e47b51eca66909d52e585874993e71013f050
Tobias Davis <accounts@davistobias.com>
2014-05-13 20:20:06 -0500

    use noddity

---
+++ js/routing.js
@@ -0,0 +1,17 @@
+var config = require('../config.js')
+
+module.exports = function(ractive) {
+	return Satnav({}).navigate({
+		path: config.pagePathPrefix + '{name}',
+		directions: function(params) {
+			ractive.set('current', params.name)
+		}
+	}).navigate({
+		path: '/',
+		directions: function(params) {
+			ractive.set('current', 'index.md')
+		}
+	}).change(function(params, old) {
+		window.scrollTo(0,0)
+	}).otherwise('/').go()
+}
